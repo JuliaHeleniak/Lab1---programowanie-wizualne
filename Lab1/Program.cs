2e154ca29c990bf4b209a98201c591981d4d9ef6
Julia Heleniak <julia.heleniak@student.put.poznan.pl>
2026-03-11 23:39:17 +0100

    dodano pierwszą linie z tekstem

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -2,8 +2,8 @@
 
 namespace HelloWordApp { 
     class Program { 
-        static void Main(string[] args) { 
-            Console.WriteLine("Hello World!"); 
+        static void Main(string[] args) {
+            Console.WriteLine("Hello, World!");
         } 
     }
 }
