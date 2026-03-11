4162722cf3898d57d745227023c730081b3ed4d0
Julia Heleniak <julia.heleniak@student.put.poznan.pl>
2026-03-11 23:44:43 +0100

    dodano trzecia komende

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -5,6 +5,7 @@ namespace HelloWordApp {
         static void Main(string[] args) {
             Console.WriteLine("Hello, World!");
             Console.WriteLine("Kolejna komenda z tekstem");
+            Console.WriteLine("Trzecia komenda z tekstem");
         } 
     }
 }
