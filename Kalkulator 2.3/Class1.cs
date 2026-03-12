81434949e58b2194a2aa9d96dd368e204ca7b41f
Julia Heleniak <julia.heleniak@student.put.poznan.pl>
2026-03-12 08:32:19 +0100

    dodano operacje arytmetyczne

--- Kalkulator 2.3/Class1.cs
+++ Kalkulator 2.3/Class1.cs
@@ -2,6 +2,26 @@
 {
     public class MathCalc
     {
-
+        public class Calculator
+        {
+            public double Add(double a, double b)
+            {
+                return a + b;
+            }
+            public double Substract(double a, double b)
+            {
+                return a - b;
+            }
+            public double Multiply(double a, double b)
+            {
+                return a * b;
+            }
+            public double Divide(double a, double b)
+            {
+                if (b == 0)
+                    throw new DivideByZeroException("Nie mozna dzielic przez zero");
+                return a / b;
+            }
+        }
     }
 }
